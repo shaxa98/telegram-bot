feb8dbbd8a137e026d3c8d00719e0e121e421c53
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-02-14 11:42:28 +0500

    shud

--- index.js
+++ index.js
@@ -7,4 +7,46 @@ bot.help((ctx) => ctx.reply("sizga qanday yordam bera olaman"));
 
 bot.hears("salom", (ctx) => ctx.reply("Voaleyko'm assalom!"));
 bot.hears("sani kim yasadi", (ctx) => ctx.reply("Shaxzod!"));
+bot.hears("yil", (ctx) => {
+  let data = new Date();
+  let year = data.getFullYear();
+  ctx.reply("hozir " + year.toString() + "-yil");
+  return ctx;
+});
+bot.hears("oy", (ctx) => {
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const data = new Date();
+  let nmonth = month[data.getMonth()];
+  ctx.reply("hozir" + nmonth.toString() + " oyi");
+  return ctx;
+});
+bot.hears("kun", (ctx) => {
+  let data = new Date();
+  let day = data.getDate();
+  ctx.reply(day.toString());
+  return ctx;
+});
+
+bot.hears("soat", (ctx) => {
+  let data = new Date();
+  let hours = data.getHours();
+  let minut = data.getMinutes();
+  ctx.reply(hours.toString() + " : " + minut.toString());
+  return ctx;
+});
+
 bot.launch();
