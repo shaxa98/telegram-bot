b6a6e881223ac6da74eb4f3294555a05578e0f77
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-02-20 12:39:07 +0500

    added lowercase

--- index.js
+++ index.js
@@ -7,50 +7,50 @@ bot.help((ctx) => ctx.reply("sizga qanday yordam bera olaman"));
 
 bot.hears("salom", (ctx) => ctx.reply("Voaleyko'm assalom!"));
 bot.hears("sani kim yasadi", (ctx) => ctx.reply("Shaxzod!"));
-bot.hears("yil", (ctx) => {
-  let data = new Date();
-  let year = data.getFullYear();
-  ctx.reply("hozir " + year.toString() + "-yil");
-  return ctx;
-});
-bot.hears("oy", (ctx) => {
-  const month = [
-    "Yanvar",
-    "Fevral",
-    "Mart",
-    "Aprel",
-    "May",
-    "Iyun",
-    "Iyul",
-    "Avgust",
-    "Sentsbr",
-    "Oktyabr",
-    "Noyabr",
-    "Dekabr",
-  ];
 
-  const data = new Date();
-  let nmonth = month[data.getMonth()];
-  ctx.reply("hozir " + nmonth.toString() + " oyi");
-  return ctx;
-});
-bot.hears("kun", (ctx) => {
-  let data = new Date();
-  let day = data.getDate();
-  ctx.reply(day.toString());
-  return ctx;
-});
-
-bot.hears("soat", (ctx) => {
-  let data = new Date();
-  let hours = data.getHours();
-  let minut = data.getMinutes();
-  ctx.reply(hours.toString() + " : " + minut.toString());
-  return ctx;
-});
 bot.on("message", (ctx) => {
   const text = ctx.update.message.text;
   const low = text.toLowerCase();
+  if (low == "soat") {
+    let data = new Date();
+    let hours = data.getHours();
+    let minut = data.getMinutes();
+    ctx.reply(hours.toString() + " : " + minut.toString());
+    return ctx;
+  }
+  if (low == "kun") {
+    let data = new Date();
+    let day = data.getDate();
+    ctx.reply(day.toString());
+    return ctx;
+  }
+  if (low == "oy") {
+    const month = [
+      "Yanvar",
+      "Fevral",
+      "Mart",
+      "Aprel",
+      "May",
+      "Iyun",
+      "Iyul",
+      "Avgust",
+      "Sentsbr",
+      "Oktyabr",
+      "Noyabr",
+      "Dekabr",
+    ];
+
+    const data = new Date();
+    let nmonth = month[data.getMonth()];
+    ctx.reply("hozir " + nmonth.toString() + " oyi");
+    return ctx;
+  }
+  if (low == "yil") {
+    let data = new Date();
+    let year = data.getFullYear();
+    ctx.reply("hozir " + year.toString() + "-yil");
+    return ctx;
+  }
 
   ctx.reply(low + ` Bu xabaringizni tushnaolmadim! Uzur  `);
 
